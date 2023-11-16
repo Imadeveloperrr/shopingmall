aae88fdb49f723ba0931cd9d607d5bb28a295d1e
Sungho <ddooochii@gmail.com>
2023-11-17 05:48:21 +0900

    test

--- src/main/java/com/example/crud/controller/IndexController.java
+++ src/main/java/com/example/crud/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.example.crud.controller;
 
+import com.example.crud.entity.Member;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class IndexController {
@@ -17,9 +20,15 @@ public class IndexController {
     public String login() {
         return "fragments/login";
     }
-    @PostMapping("/register")
+
+    @GetMapping("/register")
     public String register() {
         return "fragments/register";
     }
 
+    @PostMapping("/register") //
+    public ResponseEntity<?> registerPost(@RequestBody Member member) { // AJAX 요청은 ResponseEntity 객체가 GOOD.
+        return ResponseEntity.ok().body("Sign up Success."); // HTTP 상태 코드와 응답 본문 설정
+        // redirect:/index
+    }
 }
