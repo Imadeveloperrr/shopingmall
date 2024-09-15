39bf83045a70221520686acc5ebc8dfca7948e2c
이성호 <ddooochii@gmail.com>
2024-09-15 19:44:04 +0900

    feat : 장바구니 기능
    Cart 컨트롤러에서 productCart 페이지 보여주는 부분 작성

--- src/main/java/com/example/crud/controller/CartController.java
+++ src/main/java/com/example/crud/controller/CartController.java
@@ -1,11 +1,26 @@
 package com.example.crud.controller;
 
+import com.example.crud.data.cart.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/cart")
 public class CartController {
+
+    private static final Logger log = LoggerFactory.getLogger(CartController.class);
+    private final CartService cartService;
+
+    @GetMapping
+    public String cart() {
+        return "fragments/productCart";
+    }
+
+    
+
 }
