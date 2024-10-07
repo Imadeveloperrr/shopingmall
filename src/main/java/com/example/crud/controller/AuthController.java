54370d53e53f84c5a41b4bc66b1a0b937dd260ad
Sungho <ddooochii@gmail.com>
2024-10-07 17:00:24 +0900

    feat : 회원 로직 #3
    1. RefreshToken Entity 작성
    
    2. RefreshTokenRepository 생성
    
    3. 토큰 재발급을 위한 TokenRequestDto 생성
    
    4. AuthController 생성

---
+++ src/main/java/com/example/crud/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.crud.controller;
+
+import com.example.crud.data.member.service.MemberService;
+import com.example.crud.data.token.TokenRequestDto;
+import com.example.crud.security.JwtToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final MemberService memberService;
+
+    @RequestMapping("/reissue")
+    public ResponseEntity<JwtToken> reissue(@RequestBody TokenRequestDto tokenRequestDto) {
+        JwtToken jwtToken = memberService.reissue(tokenRequestDto);
+        return ResponseEntity.ok(jwtToken);
+    }
+}
---
+++ src/main/java/com/example/crud/entity/RefreshToken.java
@@ -0,0 +1,24 @@
+package com.example.crud.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class RefreshToken {
+
+    @Id
+    private String key; // 회원의 Email or ID
+
+    @Column(nullable = false)
+    private String token;
+
+    public void updateToken(String newToken) {
+        this.token = newToken;
+    }
+}
