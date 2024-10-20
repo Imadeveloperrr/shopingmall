1676b88be202b3174cef8205cb66e9c04bdcfef7
이성호 <ddooochii@gmail.com>
2024-10-21 00:00:12 +0900

    feat : MyBatis 상품 #4
    ㄴㅇ

--- src/main/java/com/example/crud/security/CustomUserDetailsService.java
+++ src/main/java/com/example/crud/security/CustomUserDetailsService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor // 하이
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
