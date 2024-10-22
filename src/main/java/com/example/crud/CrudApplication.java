9919ef4e7554f9072b5557afdd858c4a42df2580
이성호 <ddooochii@gmail.com>
2024-10-22 23:59:00 +0900

    feat : Member #3
    ㄱㄷ

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.example.crud.mapper")
+@MapperScan(basePackages = "com.example.crud.mapper")
 public class CrudApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/crud/data/exception/GlobarExceptionHandler.java
+++ src/main/java/com/example/crud/data/exception/GlobarExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.crud.data.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,11 @@ public class GlobarExceptionHandler {
         ErrorCode errorCode = e.getErrorCode();
         ErrorResponse response = new ErrorResponse(errorCode);
         return new ResponseEntity<>(response, errorCode.getStatus());
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        ErrorResponse response = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), "아이디 또는 비밀번호가 다릅니다.");
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 }
