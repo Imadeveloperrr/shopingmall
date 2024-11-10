92c3e91358e6654ba85d2976fc2f40e66b8f96ce
이성호 <ddooochii@gmail.com>
2024-11-11 07:24:50 +0900

    feat : Order #4
    Order DTO 츄가

--- src/main/java/com/example/crud/data/order/dto/OrderDto.java
+++ src/main/java/com/example/crud/data/order/dto/OrderDto.java
@@ -1,4 +1,20 @@
 package com.example.crud.data.order.dto;
 
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderDto {
+    private String receiverName;
+    private String receiverPhone;
+    private String receiverMobile;
+    private String receiverAddress;
+    private String deliveryMethod;
+    private String deliveryMemo;
+    private List<Long> cartItemIds; // 주문할 장바구니 아이템 ID 목록
 }
