750bb09d1f75184fb9dcd64ee796dfdd80000521
이성호 <ddooochii@gmail.com>
2024-12-12 07:50:21 +0900

    feat: BaseException 클래스 추가
    에러 발생시 표준화된 방식으로 처리하기 위한 BaseException 클래스를 추가했습니다.
    
    에러 코드: 에러의 종류를 구분하기 위한 코드
    
    에러 메시지: 사용자에게 보여줄 에러 내용
    
    추가 인자: 에러와 관련된 부가 정보

---
+++ src/main/java/com/example/crud/data/exception/BaseException.java
@@ -0,0 +1,22 @@
+package com.example.crud.data.exception;
+
+import lombok.Getter;
+
+@Getter
+public class BaseException extends RuntimeException {
+    private final ErrorCode errorCode;
+    private final String message;
+    private final Object[] args;
+
+    public BaseException(ErrorCode errorCode, Object... args) {
+        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+        this.args = args;
+    }
+
+    public BaseException(ErrorCode errorCode, String message, Object... args) {
+        this.errorCode = errorCode;
+        this.message = message;
+        this.args = args;
+    }
+}
