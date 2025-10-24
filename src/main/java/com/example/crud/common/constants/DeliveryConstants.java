cb910b1e1501ec89448b46721684d46b66210970
ddooochii <ddooochii@gmail.com>
2025-10-24 09:31:17 +0900

    feat: 배송 관련 공통 상수 추가
    - DeliveryConstants Class 생성

---
+++ src/main/java/com/example/crud/common/constants/DeliveryConstants.java
@@ -0,0 +1,22 @@
+package com.example.crud.common.constants;
+
+/**
+ * 배송 관련 공통 상수
+ */
+public class DeliveryConstants {
+
+    private DeliveryConstants() {
+
+    }
+
+    /**
+     * 무료 배송 기준 금액
+     */
+    public static final int FREE_DELIVERY_THRESHOLD = 80000;
+
+    /**
+     * 기본 배송비
+     */
+    public static final int STANDARD_DELIVERY_FEE = 3000;
+
+}
